afd0f5a5780851d7726a6e182021834bf3d948d5
julfikar-23 <julfikar.sust.swe.21@gmail.com>
2025-06-19 18:02:05 +0600

    Recent Conversations Update

---
+++ app/src/main/java/com/example/chatapp/activities/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.chatapp.activities.listeners;
+
+import com.example.chatapp.activities.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked (User user);
+}
